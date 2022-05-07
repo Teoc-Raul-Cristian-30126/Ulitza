d0c23832e559c9c66f60028b1fb55b25c2440c35
Teoc Raul-Cristian <teoccristian2002@yahoo.com>
2022-05-07 19:48:35 +0300

    C4

---
+++ c4-samples/src/main/java/aut/utcluj/isp/ex1/Exercise1.java
@@ -0,0 +1,7 @@
+package aut.utcluj.isp.ex1;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ c4-samples/src/main/java/aut/utcluj/isp/ex2/Exercise2.java
@@ -0,0 +1,7 @@
+package aut.utcluj.isp.ex2;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+
+    }
+}
